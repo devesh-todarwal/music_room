7b412bbe0fca3d72a456fdec294c265e82ff3682
Devesh Todarwal <todarwal.devesh@gmail.com>
2020-12-02 00:19:42 +0530

    Creating API View

--- api/models.py
+++ api/models.py
@@ -1,9 +1,20 @@
 from django.db import models
-
+import random
+import string
 # Create your models here.
 
+def generate_code():
+    length = random.randint(4,8)
+    
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+        return code
+
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True)
+    code = models.CharField(max_length=8, default='', unique=True)
     host = models.CharField(max_length=30, unique=True)
     guest_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
