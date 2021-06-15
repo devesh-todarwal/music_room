19f225c3498b3eba338afda796b99aa1b45df8ae
Devesh Todarwal <todarwal.devesh@gmail.com>
2021-06-15 23:53:53 +0530

    Revamping the App

--- music_room/music_room/settings.py
+++ music_room/music_room/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0bq4x33knzuvgm#rl=w18x17ybwh%xy+b-9+d9$19yz^wr6((('
+SECRET_KEY = '+6%e!#euly@%tc&rc2kb*s8gh4zj=soh7t%=b8$ahc323skr8w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'rest_framework'
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
