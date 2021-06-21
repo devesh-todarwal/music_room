7effaafd89f2744c555d4dde03c665f35d6a1934
Devesh Todarwal <todarwal.devesh@gmail.com>
2021-06-21 23:41:44 +0530

    Adding Material-UI Elements

--- music_room/frontend/src/components/CreateRoomPage.js
+++ music_room/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,27 @@
 import React, {Component} from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p> This is the Create Room Page!</p>
+        return <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+            <Typography></Typography>
+        </Grid>
+        </Grid>;
     }
 }
