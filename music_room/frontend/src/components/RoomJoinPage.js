b32ed6f655db4519167021c1dc55450a9fc5ad27
Devesh Todarwal <todarwal.devesh@gmail.com>
2021-06-23 16:23:29 +0530

    adding create room interface

--- music_room/frontend/src/components/RoomJoinPage.js
+++ music_room/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,47 @@
 import React, {Component} from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: "",
+            error: ""
+        }
     }
 
     render() {
-        return <p> This is the Room Join Page!</p>
+        return(
+            <Grid container spacing={1}>
+                <Grid item xs = {12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join Room
+                    </Typography>
+                </Grid>
+
+                <Grid item xs = {12} align="center">
+                    <TextField 
+                        error = {this.state.error}
+                        label = "Code"
+                        placeholder = "Enter a Room Code"
+                        value = { this.state.roomCode }
+                        helperText = { this.state.error }
+                        variant = "outlined"
+                    />
+                </Grid>
+                <Grid item xs = {12} align="center">
+                    <Button variant="contained" color="primary" onClick>
+                    Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs = {12} align="center">
+                    <Button variant="contained" color="secondary" to = "/" component={ Link }>
+                    Back
+                    </Button>
+
+                </Grid>
+            </Grid>
+        )
     }
 }
